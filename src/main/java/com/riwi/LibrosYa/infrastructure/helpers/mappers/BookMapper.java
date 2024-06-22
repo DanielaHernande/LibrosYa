8eb4c75f63ed62a0e20078bcb9453ec0572772b7
DanielaHernande <danielajimenezhernandez6@gmail.com>
2024-06-22 16:11:51 -0500

    Feat: Create mapper Book

--- src/main/java/com/riwi/LibrosYa/infrastructure/helpers/mappers/BookMapper.java
+++ src/main/java/com/riwi/LibrosYa/infrastructure/helpers/mappers/BookMapper.java
@@ -2,25 +2,26 @@ package com.riwi.LibrosYa.infrastructure.helpers.mappers;
 
 import java.util.List;
 
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
-import com.riwi.LibrosYa.api.dto.request.UserRequest;
-import com.riwi.LibrosYa.api.dto.response.UserResponse;
-import com.riwi.LibrosYa.domain.entities.UserEntity;
+import com.riwi.LibrosYa.api.dto.request.BookRequest;
+import com.riwi.LibrosYa.api.dto.response.BookResponse;
+import com.riwi.LibrosYa.domain.entities.Book;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
-public interface UserMapper {
-    
-    UserResponse toUserResponse(UserEntity userEntity);
+public interface BookMapper {
 
+   //BookResponse toBookResponse(Book book);
+    
     // Para ignorar
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "loans", ignore = true)
     @Mapping(target = "reservations", ignore = true)
 
-    UserEntity toUserEntity(UserRequest userRequest);
+    Book toBookEntity(BookRequest bookRequest);
 
-    List<UserResponse> UserListToResponseList(List<UserRequest> userRequest);
+    //List<BookResponse> BookListToResponseList(List<Book> bookRequests); 
 }
