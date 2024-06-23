98d80a2296317343e5ac53a21afc053b9e2afd21
DanielaHernande <danielajimenezhernandez6@gmail.com>
2024-06-22 20:20:54 -0500

    Feat: Create Services User

---
+++ src/main/java/com/riwi/LibrosYa/infrastructure/services/UserServices.java
@@ -0,0 +1,33 @@
+package com.riwi.LibrosYa.infrastructure.services;
+
+import com.riwi.LibrosYa.api.dto.request.UserRequest;
+import com.riwi.LibrosYa.api.dto.response.UserResponse;
+import com.riwi.LibrosYa.infrastructure.abstrac_services.IUserServices;
+
+public class UserServices implements IUserServices{
+
+    @Override
+    public UserResponse create(UserRequest request) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    }
+
+    @Override
+    public UserResponse get(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    public UserResponse update(UserRequest request, Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void delete(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+    
+}
